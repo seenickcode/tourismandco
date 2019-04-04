9a6b66dee03c80ad574ebe6dec4ef9c734d7d42f
Nick Manning <nicholas.manning@gmail.com>
2019-04-04 09:08:56 -0400

    Removed style.

--- lib/app.dart
+++ lib/app.dart
@@ -2,17 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'screens/location_detail/location_detail.dart';
-import 'style.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LocationDetail(),
-      theme: ThemeData(
-          textTheme: TextTheme(
-        body1: Body1Style,
-      )),
     );
   }
 }
